f3750ae8bd3c6a153f687dbaa2e80c3377622494
Kwabena Duku <64412821+Kwabenaduku@users.noreply.github.com>
2024-08-25 22:55:38 -0400

    Update init.R

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("randomForest","tidyverse","MASS","caret", "data.table")
+my_packages = c("randomForest","tidyverse","MASS","caret", "data.table","shinythemes")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
