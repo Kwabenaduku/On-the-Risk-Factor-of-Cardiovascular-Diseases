968348db63c32a510a2ae168ece355ced46761df
Kwabena Duku <64412821+Kwabenaduku@users.noreply.github.com>
2024-10-08 21:28:09 -0400

    Update ui.R
    Updated Background

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ library(shinythemes)
 TrainSet <- read.csv("training.csv", header = TRUE)
 TrainSet <- TrainSet[,-15]
 
-ui <- fluidPage(theme = shinytheme("superhero"),
+ui <- fluidPage(theme = shinytheme("cosmo"),
                 
                 # Application title
                 titlePanel("Cardiovascular Risk Predictor"),
